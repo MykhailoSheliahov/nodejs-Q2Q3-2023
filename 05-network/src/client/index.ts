947ad628eeb4af3f2bc932a6fb9b784335714095
Mykhailo Sheliahov <30924289+MykhailoSheliahov@users.noreply.github.com>
2023-05-31 19:09:42 +0300

    [HW-05] - Network: add REST API for users and their hobbies (#5)
    Co-authored-by: Mykhailo Sheliahov

---
+++ 05-network/src/client/index.ts
@@ -0,0 +1,55 @@
+import { userService } from "./services/userService";
+
+(async () => {
+   try {
+
+      //1
+      const data = await userService.createUser({
+         name: "Oleh",
+         email: 'Oleh@google.com',
+         hobbies: ['chess', 'pool', ],
+      });
+      // const data = await userService.createUser({
+      //    name: "",
+      //    email: '',
+      //    hobbies: [],
+      // });
+      // const data = await userService.deleteUser(2);
+      // const data = await userService.deleteUser(3);
+
+      //2
+      // const data = await userService.updateUser({
+      //    id:2,
+      //    name: "ivan",
+      //    email: 'ivan@google.com',
+      //    hobbies: ['ivan', 'ivan1', 'ivan2'],
+      // });
+      // const data = await userService.updateUser({
+      //    id:2,
+      // });
+
+      //3
+      // const data = await userService.getUserById(2);
+      // const data = await userService.getUserById(4);
+
+      // const data = await userService.getUsers();
+
+      //4
+      //  await userService.addUserHobby({
+      //    id:1,
+      //    hobby: "basketball"
+      //  });
+      // await userService.deleteUserHobby({
+      //    id: 1,
+      //    hobby: "dancing"
+      // });
+
+      // const data = await userService.getUserHobbies(1);
+
+      console.log("🚀 ~ file: index.ts:5 ~ data:", data)
+   } catch (e: any) {
+      console.log("statusCode", e?.response.status)
+      console.log("statusText", e?.response.statusText)
+      console.log("message", e?.response.headers.message)
+   }
+})();
