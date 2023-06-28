a508dddc0e32910c675a55939e32fc8b6965ad2f
Mykhailo Sheliahov <mykhailo.sheliahov@abstract.it>
2023-06-28 15:19:35 +0300

    feat: this will fail

--- 06-express/server/controllers/cartController.ts
+++ 06-express/server/controllers/cartController.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-import { dbController } from './../controllers/dbController';
-import { CartConnector } from '../connectors/cartConnector';
-import { OrderController } from './orderController';
-import { User } from '../types';
+import { dbController } from "./../controllers/dbController";
+import { CartConnector } from "../connectors/cartConnector";
+import { OrderController } from "./orderController";
+import { User } from "../types";
 
 export class CartController {
   static async getCart(req: Request, res: Response) {
@@ -11,42 +11,43 @@ export class CartController {
 
     const userCart = await dbController.getUserCart({
       userId: id!,
-    })
+    });
 
     const total = CartConnector.calcTotal(userCart);
     const cart = CartConnector.hideUserProps(userCart);
 
     res.send({
       statusCode: 200,
-      message: 'Success',
+      message: "Success",
       data: {
         cart,
-        total
-      }
-    })
-  };
+        total,
+      },
+    });
+  }
 
   static async updateCart(req: Request, res: Response) {
     const { id } = req.query.user as unknown as User;
-    const { items } = req.body
+    const { items } = req.body;
 
     const userCart = await dbController.updateCart({
       userId: id!,
-      data: items
+      data: items,
     });
-    
+
     const total = CartConnector.calcTotal(userCart);
     const cart = CartConnector.hideUserProps(userCart);
 
     res.send({
       statusCode: 200,
-      message: 'Success',
+      message: "Success",
+
       data: {
         cart,
-        total
-      }
-    })
-  };
+        total,
+      },
+    });
+  }
 
   static async deleteCart(req: Request, res: Response) {
     const { id } = req.query.user as unknown as User;
@@ -56,12 +57,12 @@ export class CartController {
     });
 
     res.send({
-      'statusCode': 200,
-      'message': `Cart for '${id}' successfully deleted`,
+      statusCode: 200,
+      message: `Cart for '${id}' successfully deleted`,
     });
-  };
+  }
 
   static async createOrder(req: Request, res: Response) {
     await OrderController.createOrder(req, res);
-  };
-};
+  }
+}
